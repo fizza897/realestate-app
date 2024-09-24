fdc34e8b4b530a9946ffbe686db4c9d587694357
fizza897 <fizzaazam6@gmail.com>
2024-09-24 22:27:44 +0500

    Login-Panel

---
+++ src/app/components/LoginPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPanel = () => {
+  return (
+    <div>LoginPanel</div>
+  )
+}
+
+export default LoginPanel
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -24,14 +24,7 @@ import { HomeModernIcon } from '@heroicons/react/24/solid'
         
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
+       
       </NavbarContent>
     </Navbar>
   );
