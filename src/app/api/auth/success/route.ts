606a695653b522915261312ba6b166a5924b8cad
fizza897 <fizzaazam6@gmail.com>
2024-09-25 03:13:31 +0500

    logout-integration

--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
 
 export async function Get(){
     const {getUser} =await getKindeServerSession()
@@ -22,5 +23,6 @@ export async function Get(){
                 email:user.email ?? ""
             }
         })
+        return NextResponse.redirect("http://localhost:3000/")
     }
 }
--- src/app/components/LoginPanel.tsx
+++ src/app/components/LoginPanel.tsx
@@ -1,5 +1,6 @@
-import { getKindeServerSession, LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { Button } from '@nextui-org/react'
+import {RegisterLink, LoginLink,LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import React from 'react'
 
 const SignInPanel = async () => {
@@ -8,7 +9,12 @@ const SignInPanel = async () => {
   const user =await getUser()
   
   if(await isAuthenticated())
-    return <div>{user?.given_name}</div>
+    return <div className='gap-5 inline-flex justify-center'>{user?.given_name}
+  
+    <LogoutLink>Logout</LogoutLink>
+
+
+    </div>
 
 
   return (
