06889bdfb125fe132447f086fab18dbdbff98726
fizza897 <fizzaazam6@gmail.com>
2024-09-24 22:51:46 +0500

    login-panel

--- src/app/components/LoginPanel.tsx
+++ src/app/components/LoginPanel.tsx
@@ -1,8 +1,25 @@
+import { getKindeServerSession, LoginLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { Button } from '@nextui-org/react'
 import React from 'react'
 
-const LoginPanel = () => {
+const LoginPanel = async () => {
+    const {isAuthenticated,getUser} =await getKindeServerSession()
+    const user = await getUser()
+    if(await isAuthenticated())
+         return 
+        <div>{user?.given_name}</div>
   return (
-    <div>LoginPanel</div>
+    <>
+    <div className='flex gap-3'>
+            <Button color='primary'>
+                <LoginLink>Login</LoginLink>
+            </Button>
+            <Button color='primary'>
+                <LoginLink>Register</LoginLink>
+            </Button>
+    </div>
+    
+    </>
   )
 }
 
